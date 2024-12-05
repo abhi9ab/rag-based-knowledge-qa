675d58c79c9bddfe6e9cc49075cba2e8e4c1cc70
abhi9ab <abhinabdas004@gmail.com>
2024-12-05 07:26:31 +0000

    add image compression

--- components/ReportComponent.tsx
+++ components/ReportComponent.tsx
@@ -1,16 +1,13 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 import { useToast } from "@/hooks/use-toast"
 
-type Props = {
-  onReportConfirmation: (data: string) => void
-}
-
-const ReportComponent = ({ onReportConfirmation }: Props) => {
+const ReportComponent = () => {
   const { toast } = useToast()
+  const [base64Data, setBase64Data] = useState('')
 
   function handleReportSelection(event: ChangeEvent<HTMLInputElement>): void {
     if (!event.target.files) return;
@@ -33,11 +30,100 @@ const ReportComponent = ({ onReportConfirmation }: Props) => {
         });
         return;
       }
+
+      if (isValidImage) {
+        compressImage(file, (compressedFile) => {
+          const reader = new FileReader();
+
+          reader.onloadend = () => {
+            const base64String = reader.result as string;
+            setBase64Data(base64String);
+            console.log(base64String);
+          };
+
+          reader.readAsDataURL(compressedFile);
+        });
+      }
+
+      if (isValidDoc) {
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+          const base64String = reader.result as string;
+          setBase64Data(base64String);
+          console.log(base64String);
+        };
+        reader.readAsDataURL(file);
+      }
     }
   }
 
-  function extractDetails(): void {
-    throw new Error('Function not implemented.')
+  async function extractDetails(): void {
+    if (!base64Data) {
+      toast({
+        variant: 'destructive',
+        description: "Upload a valid report!",
+      });
+      return;
+    }
+
+    const response = await fetch("api/extractreportgemini", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        base64: base64Data,
+      }),
+    });
+
+    if (response.ok) {
+      const reportText = await response.text();
+      console.log(reportText);
+    }
+
+  }
+  
+  function compressImage(file: File, callback: (compressedFile: File) => void) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const img = new Image();
+      img.onload = () => {
+        // Create a canvas element
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        // Set  canvas dimensions to match the image
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Draw the image onto the canvas
+        ctx!.drawImage(img, 0, 0);
+
+
+        // Apply basic compression (adjust quality as needed)
+        const quality = 0.1; // Adjust quality as needed
+
+        // Convert canvas to data URL
+        const dataURL = canvas.toDataURL('image/jpeg', quality);
+
+        // Convert data URL back to Blob
+        const byteString = atob(dataURL.split(',')[1]);
+        const ab = new ArrayBuffer(byteString.length);
+        const ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+
+        }
+        const compressedFile = new File([ab], file.name, { type: 'image/jpeg' });
+
+        callback(compressedFile);
+      };
+      img.src = e.target!.result as string;
+    };
+
+    reader.readAsDataURL(file);
   }
 
   return (
@@ -56,7 +142,7 @@ const ReportComponent = ({ onReportConfirmation }: Props) => {
           variant="destructive"
           className="bg-[#D90013]"
         >
-          2. Looks Good
+          Looks Good
         </Button>
       </fieldset>
     </div>
