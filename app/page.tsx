b6f3d550c6c544266a1b60772b767785810410b2
abhi9ab <abhinabdas004@gmail.com>
2024-12-05 06:09:48 +0000

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import React from 'react'
 
-type Props = {}
-
-const HomeComponent = (props: Props) => {
+const HomeComponent = () => {
   return (
-    <div>HomeComponent</div>
+    <div className="grid h-screen w-full">
+      <div className="flex flex-col">
+        <header className="sticky top-0 z-10 flex h-[57px] bg-background items-center gap-1 border-b px-4">
+          <h1 className="text-xl font-semibold">
+            <span className="flex flex-row">Sage</span>
+          </h1>
+          <div className="w-full flex flex-row justify-end gap-2">
+            <ModeToggle />
+          </div>
+        </header>
+      </div>
+    </div>
+
   )
 }
 
