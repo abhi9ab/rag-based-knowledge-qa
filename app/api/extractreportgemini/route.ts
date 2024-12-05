8f3d4bef32cb5d55e1ecba03fc7f680647e451fc
abhi9ab <abhinabdas004@gmail.com>
2024-12-05 07:41:59 +0000

    add gemini api

---
+++ app/api/extractreportgemini/route.ts
@@ -0,0 +1,33 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+});
+
+const prompt = `Attached is an image or PDF of a legal document. 
+Go over the legal document and identify key legal provisions, critical clauses, and potential implications. Then summarize in 100 words. You may increase the word limit if the document has multiple pages. Do not output specific names of parties involved. Make sure to include numerical values and key details from the document, including document title.
+## Summary: `;
+
+export async function POST(req: Request, res: Response) {
+    const { base64 } = await req.json();
+    const filePart = fileToGenerativePart(base64)
+
+    console.log(filePart);
+    const generatedContent = await model.generateContent([prompt, filePart]);
+
+    console.log(generatedContent);
+    const textResponse = generatedContent.response.candidates![0].content.parts[0].text;
+    return new Response(textResponse, { status: 200 })
+}
+
+function fileToGenerativePart(imageData: string) {
+    return {
+        inlineData: {
+            data: imageData.split(",")[1],
+            mimeType: imageData.substring(
+                imageData.indexOf(":") + 1,
+                imageData.lastIndexOf(";")
+            ),
+        },
+    }
+}
